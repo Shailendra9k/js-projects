8b324c95a01b33edff2b824cfade45f8b1c56396
Shailendra Kushwaha <learning4succedding@gmail.com>
2019-07-29 10:08:08 +0300

    project

--- photo-slider/app.js
+++ photo-slider/app.js
@@ -1,1 +1,17 @@
-console.log("Hello");
+var pics = [
+  "images/img_1.jpg",
+  "images/img_2.jpg",
+  "images/img_3.jpg",
+  "images/img_4.jpg",
+  "images/img_5.jpg",
+  "images/img_6.jpg"
+];
+/*Notes:
+-HTML is the structure.
+-css is the style.
+
+JavaScript is the whole bunch of things.
+-It's the interactivity.
+-It's the loading of new data.
+-It's the sending of HTTP request.
+-It's what allows us to make games.*/
