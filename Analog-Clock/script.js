0118237f05105bb4b4eba4a9be4b0f0c9ba1fb1c
Shailendra Kushwaha <learning4succedding@gmail.com>
2019-08-05 13:06:08 +0300

    Project-ongoing

--- Analog-Clock/script.js
+++ Analog-Clock/script.js
@@ -1,3 +1,7 @@
 const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
+
+let hrPosition;
+let minPosition;
+let secPosition;
