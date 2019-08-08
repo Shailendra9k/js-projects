9f2ed9c87b5165765285bdedb40dcb537917237b
Shailendra Kushwaha <learning4succedding@gmail.com>
2019-08-08 08:15:28 +0300

    form handling

--- Password-Generator/script.js
+++ Password-Generator/script.js
@@ -1,4 +1,4 @@
 //In this proejct we'll create random password generator
 //After putting specified number one can get a unique password with letter, symbol and numbers
 
-console.log("hello");
+var keyList = 'abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*"';
